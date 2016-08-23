6a577f1497356b3af9e0d15f91745b3d3e3b812d
liuqinghua <liuqinghua@ppdai.com>
2016-08-23 10:47:01 +0800

    Signed-off-by: liuqinghua <liuqinghua@ppdai.com>

--- src/test/java/per/liuqh/dao/UserDaoTest.java
+++ src/test/java/per/liuqh/dao/UserDaoTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import per.liuqh.common.TestBaseCase;
 import per.liuqh.ssm.dao.UserMapper;
 import per.liuqh.ssm.entity.User;
+import per.liuqh.ssm.entity.UserExample;
 
 public class UserDaoTest extends TestBaseCase{
 	
@@ -16,6 +17,7 @@ public class UserDaoTest extends TestBaseCase{
 	public void test1(){
 		User u=userMapper.selectByPrimaryKey(1);
 		System.out.println(u);
-		//System.out.println(userMapper.countAll());
+		
+		System.out.println(userMapper.countByExample(new UserExample()));
 	}
 }
